2e8e0ab248a4f928c35f38bd46d3ceba986a74b0
piyushGupta-001 <piyushkumargupta842001@gmail.com>
2024-10-05 01:42:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import ThankYou from "./components/ThankYou";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CartProvider } from "react-use-cart";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <CartProvider>
+                <ProductList />
+              </CartProvider>
+            }
+          ></Route>
+          <Route
+            exact
+            path="/cart"
+            element={
+              <CartProvider>
+                <Cart />
+              </CartProvider>
+            }
+          ></Route>
+          <Route
+            exact
+            path="/thankyou"
+            element={
+              <CartProvider>
+                <ThankYou />
+              </CartProvider>
+            }
+          ></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
