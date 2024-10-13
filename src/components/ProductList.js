96fc2a6e93743649be8d718a1c6bb24acc43fb07
piyushGupta-001 <piyushkumargupta842001@gmail.com>
2024-10-14 01:00:19 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,6 +3,7 @@ import DataTable from "react-data-table-component";
 import productData from "./products";
 import { useCart } from "react-use-cart";
 import { Link } from "react-router-dom";
+import "./responsive.css";
 
 function ProductList() {
   const [search, setSearch] = useState("");
@@ -42,11 +43,13 @@ function ProductList() {
     {
       name: "Image",
       selector: (row) => (
-        <img
-          style={{ height: "50px", width: "50px" }}
-          src={row.image}
-          alt={row.name}
-        />
+        <div className="fixed-image-column">
+          <img
+            style={{ height: "50px", width: "50px", objectFit: "cover" }}
+            src={row.image}
+            alt={row.name}
+          />
+        </div>
       ),
     },
     {
@@ -100,12 +103,29 @@ function ProductList() {
     {
       name: "Buy",
       cell: (row) => (
-        <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+
+            gap: "10px",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <input
-            style={{ width: "60px", height: "33px", borderRadius: "0px" }}
-            className="form-control me-1"
+            style={{
+              maxWidth: "40px",
+              height: "35px",
+              borderRadius: "0px",
+              textAlign: "center",
+              boxSizing: "border-box",
+              flex: "1",
+              marginBottom: "5px",
+              fontSize: "14px",
+            }}
             type="number"
-            value={quantity[row.id - 1].qty}
+            value={quantity[row.id - 1]?.qty || ""}
             onChange={(event) => {
               let inputValue = parseInt(event.target.value);
               let updatedQty = [...quantity];
@@ -114,63 +134,76 @@ function ProductList() {
               setQuantity(updatedQty);
             }}
           />
+
           <button
             style={{
-              width: "60px",
-              height: "30px",
+              width: "100%",
+              maxWidth: "140px",
+              height: "40px",
               borderRadius: "0px",
-              paddingBottom: "25px",
+              backgroundColor: "#343a40",
+              color: "white",
+              border: "none",
+              fontSize: "14px",
+              cursor: "pointer",
+              marginBottom: "5px",
             }}
-            className="btn btn-dark me-2"
-            onClick={() => {
-              // This onClick handler is now tied to the checkbox
-            }}
+            onClick={() => {}}
           >
             <i className="fa-solid fa-cart-shopping fa-xs"></i>
           </button>
+
           <input
             type="checkbox"
             key={row.id}
             id={row.id}
+            style={{
+              marginTop: "5px",
+              height: "20px",
+              width: "20px",
+              flexShrink: "1",
+              marginRight: "10px",
+              marginBottom: "5px",
+            }}
             disabled={
-              quantity[row.id - 1].qty < 1 ||
-              row.totalAvailableQuantity <= quantity[row.id - 1].qty 
+              quantity[row.id - 1]?.qty < 1 ||
+              row.totalAvailableQuantity <= quantity[row.id - 1]?.qty
             }
             onChange={() => {
-              if (quantity[row.id - 1].qty < 1) {
+              if (quantity[row.id - 1]?.qty < 1) {
                 showAlert(
                   "Product quantity should be greater than 0",
                   "alert-danger"
                 );
-                return; // Don't add to cart if quantity is invalid
+                return;
               } else if (
-                quantity[row.id - 1].qty > row.totalAvailableQuantity
+                quantity[row.id - 1]?.qty > row.totalAvailableQuantity
               ) {
                 showAlert(
                   "Specified quantity of product is not available",
                   "alert-danger"
                 );
-                return; // Don't add to cart if quantity is invalid
+                return;
               }
 
               if (items.some((item) => item["id"] === row.id)) {
                 let itemIndex = items.findIndex((x) => x.id === row.id);
                 if (
-                  quantity[row.id - 1].qty >
+                  quantity[row.id - 1]?.qty >
                   items[itemIndex]["updatedAvailableQuantity"]
                 ) {
                   showAlert(
                     "Specified quantity of product is not available",
                     "alert-danger"
                   );
-                  return; // Don't add to cart if quantity is invalid
+                  return;
                 } else {
                   updateItem(row.id, {
                     quantity:
-                      items[itemIndex]["quantity"] + quantity[row.id - 1].qty,
+                      items[itemIndex]["quantity"] + quantity[row.id - 1]?.qty,
                     updatedAvailableQuantity:
                       items[itemIndex]["updatedAvailableQuantity"] -
-                      quantity[row.id - 1].qty,
+                      quantity[row.id - 1]?.qty,
                   });
                   showAlert(
                     "Product quantity updated in cart",
@@ -185,15 +218,15 @@ function ProductList() {
                     name: row.name,
                     price: row.price,
                     updatedAvailableQuantity:
-                      row.totalAvailableQuantity - quantity[row.id - 1].qty,
+                      row.totalAvailableQuantity - quantity[row.id - 1]?.qty,
                   },
-                  quantity[row.id - 1].qty
+                  quantity[row.id - 1]?.qty
                 );
                 showAlert("Product added to cart", "alert-success");
               }
             }}
           />
-        </>
+        </div>
       ),
     },
   ];
@@ -282,18 +315,42 @@ function ProductList() {
         id="alert"
         role="alert"
       ></div>
-      <DataTable
-        columns={columns}
-        data={filteredProducts}
-        fixedHeader
-        selectableRowsHighlight
-        highlightOnHover
-        subHeader
-        subHeaderComponent={
-          <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          "@media (max-width: 768px)": {
+            display: "flex",
+            flexDirection: "column",
+          },
+          justifyContent: "space-between",
+          alignItems: "center",
+
+          padding: "10px",
+          flexWrap: "wrap",
+          gap: "30px",
+        }}
+      >
+        <div
+          style={{
+            flexWrap: "wrap", // Allow wrapping of elements on smaller screens
+            gap: "10px", // Space between elements
+            justifyContent: "space-between",
+            // border: "1px solid maroon",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row" }}>
             <select
               className="form-select me-1"
-              style={{ width: "140px", borderRadius: "0px" }}
+              style={{
+                width: "100%",
+                maxWidth: "140px", // Limit the width on smaller screens
+                borderRadius: "0px",
+                boxSizing: "border-box",
+              }}
               name="category"
               id="category"
               value={categoryFilter}
@@ -305,9 +362,15 @@ function ProductList() {
               <option value="hoodie">Hoodie</option>
               <option value="t-shirt">T-Shirt</option>
             </select>
+
             <select
               className="form-select me-3"
-              style={{ width: "140px", borderRadius: "0px" }}
+              style={{
+                width: "100%",
+                maxWidth: "140px", // Limit the width on smaller screens
+                borderRadius: "0px",
+                boxSizing: "border-box",
+              }}
               name="size"
               id="size"
               value={sizeFilter}
@@ -322,8 +385,17 @@ function ProductList() {
               <option value="XL">XL</option>
               <option value="XXL">XXL</option>
             </select>
+          </div>
+
+          <div>
             <span
-              style={{ color: "blue", cursor: "pointer" }}
+              style={{
+                color: "blue",
+                cursor: "pointer",
+                // border: "1px solid blue",
+                // marginRight: "10px",
+                // marginTop: "5px",
+              }}
               onClick={() => {
                 setFilteredProducts(productData);
                 setCategoryFilter("");
@@ -332,25 +404,75 @@ function ProductList() {
             >
               <i className="fa-solid fa-arrow-rotate-left"></i> Reset
             </span>
-            <span style={{ marginLeft: "440px" }}>Search:</span>{" "}
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+
+            "@media (max-width: 768px)": {
+              display: "flex",
+              flexDirection: "column",
+              flexWrap: "wrap",
+            },
+            gap: "10px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span style={{ color: "blue" }}>Search:</span>
             <input
               type="text"
-              style={{ width: "200px", borderRadius: "0px" }}
+              style={{}}
               className="form-control ms-1 me-3"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
-            <Link
-              className="btn btn-primary"
-              to="/cart"
-              style={{ borderRadius: "0px" }}
-            >
-              Add to Cart
-            </Link>
-          </>
-        }
-        subHeaderAlign="left"
-      />
+          </div>
+
+          <Link
+            className="btn btn-primary"
+            to="/cart"
+            style={{
+              borderRadius: "0px",
+              whiteSpace: "nowrap",
+              width: "100%",
+              maxWidth: "150px",
+              padding: "10px 20px",
+              textAlign: "center",
+              boxSizing: "border-box",
+            }}
+          >
+            Add to Cart
+          </Link>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        <DataTable
+          columns={columns}
+          data={filteredProducts}
+          fixedHeader
+          selectableRowsHighlight
+          highlightOnHover
+          subHeader
+          subHeaderAlign="left"
+        />
+      </div>
     </div>
   );
 }
