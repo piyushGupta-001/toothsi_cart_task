f56f4e3ba9b54b97b34e007b77669b4b2129ed3a
piyushGupta-001 <piyushkumargupta842001@gmail.com>
2024-10-14 01:01:22 +0530

    Update ThankYou.js

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -12,23 +12,38 @@ const ThankYou = () => {
   return (
     <div
       style={{
-        margin: "180px",
+        margin: "20px auto",
         padding: "25px",
-        borderRadius: "30px",
+        borderRadius: "20px",
         boxShadow: "0 2px 10px rgb(0 0 0 / 0.2)",
+        maxWidth: "600px",
+        width: "100%",
+        textAlign: "center",
+        boxSizing: "border-box",
       }}
     >
-      <h1 style={{ textAlign: "center" }}>
+      <h1
+        style={{
+          fontSize: "24px",
+          marginBottom: "20px",
+        }}
+      >
         Thank You! Your order has been successfully placed!
       </h1>
+
       <Link
-        className="btn btn-primary mt-2 pt-3 pb-3"
+        className="btn btn-primary"
         to="/"
         style={{
+          display: "inline-block",
           height: "60px",
           borderRadius: "30px",
-          width: "200px",
-          marginLeft: "450px",
+          width: "100%",
+          maxWidth: "200px",
+          padding: "15px",
+          boxSizing: "border-box",
+          textAlign: "center",
+          margin: "0 auto",
         }}
       >
         Go to Homepage
